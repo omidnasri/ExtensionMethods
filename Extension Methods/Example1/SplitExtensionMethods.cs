852a150d2a8e96031788bf16de1a368503999586
Omid Nasri <omid.nasri@outlook.com>
2015-11-27 12:05:26 +0330

    Update : 11-27-2015

---
+++ Extension Methods/Example1/SplitExtensionMethods.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Extension_Methods.Example1
+{
+    public static class SplitExtensionMethods
+    {
+        public static string[] SplitByComma(this String s)
+        {
+            return s.Split(',');
+        }
+    }
+}
---
+++ Extension Methods/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Extension_Methods.Example1;
+
+namespace Extension_Methods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = "string1,string2,string3";
+            var result = input.SplitByComma();
+            Console.WriteLine(result[1]);
+        }
+    }
+}
